5fb7f86773a845e71af3fc80b26e4bd1afa1b58e
behlock <behlocks@gmail.com>
2023-08-01 11:54:22 +0100

    move stats tracking url to env var

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -1,4 +1,5 @@
 export const config: { [key: string]: string } = {
+  STATS_TRACKING_URL: process.env.NEXT_PUBLIC_STATS_TRACKING_URL || '',
   GRAPHQL_URL: process.env.NEXT_PUBLIC_GRAPHQL_URL || '',
   MAPBOX_ACCESS_TOKEN: process.env.NEXT_PUBLIC_MAPBOX_ACCESS_TOKEN || '',
 }
